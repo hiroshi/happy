4a087d61cd5d3fd41c907a161db3101868814b6f
Hiroshi Saito <hiroshi3110@gmail.com>
2009-03-21 22:26:27 +0900

    * tidy up routes

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,4 +1,7 @@
 class HostsController < ApplicationController
+  def index
+  end
+
   def show
     @host = Person.find_by_identity_url(params[:identity_url])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+  # hosts
   map.root :controller => "hosts"
-  map.resources :hosts, :except => :show
+  map.resources :hosts, :only => :index
   map.host "host", :controller => "hosts", :action => "show"
   map.host_loader "loader.js", :controller => "hosts", :action => "loader", :format => :js
 
-  map.resource :session
-
-  map.resources :people
-
-  map.resources :sites
+  map.resource :session, :only => [:new, :create]
 
   map.resources :happies, :only => [:create]
 end
